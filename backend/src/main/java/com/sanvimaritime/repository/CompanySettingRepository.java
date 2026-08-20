c8bde6a9bf7299e2fb0f2319a6a66b69191f6cb1
unknown <snellbhor@gmail.com>
2026-08-20 23:25:09 +0530

    Clean repository root and preserve Plant Global project

--- backend/src/main/java/com/sanvimaritime/repository/CompanySettingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sanvimaritime.repository;
-
-import com.sanvimaritime.entity.CompanySetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanySettingRepository extends JpaRepository<CompanySetting, String> {
-}
--- backend/src/main/java/com/sanvimaritime/repository/MarineServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sanvimaritime.repository;
-
-import com.sanvimaritime.entity.MarineServiceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarineServiceRepository extends JpaRepository<MarineServiceEntity, String> {
-}
